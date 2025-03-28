9e48d8b47d51e201a2fff54353e300b7fe949e1c
Eunice <reyes05eunice@gmail.com>
2025-03-28 21:11:05 +0800

    Prints first letter of each word in uppercase

--- no_title.py
+++ no_title.py
@@ -5,3 +5,17 @@
 string = input("Enter a sentence: ")
 
 # Convert all first letters of each word to uppercase without title()
+
+def title_alternative():
+    result = []
+    word = string.split()
+
+    for char in word:
+        if len(char) > 0:
+            result.append(char[0].upper() + char[1:].lower())
+        else:
+            result.append(char)
+        
+    return " ".join(result)
+
+print(title_alternative())
