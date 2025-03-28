f59d876ac54ddcce844ce3b66d8a131500a2712b
Eunice <reyes05eunice@gmail.com>
2025-03-28 20:41:00 +0800

    Checks if ALL characters are uppercase w/o isupper()

--- no_isupper.py
+++ no_isupper.py
@@ -5,3 +5,11 @@
 string = input("Enter a sentence: ")
 
 # Print 'True' if string is in uppercase without isupper()
+
+def isupper_alternative(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            return False
+    return all('A' <= char <= 'Z' for char in s)
+
+print("Are all characters in uppercase?", isupper_alternative(string))
