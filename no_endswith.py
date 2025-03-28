42ca91762d9a78784461d5ded3695575d9178245
Eunice <reyes05eunice@gmail.com>
2025-03-28 20:28:56 +0800

    Checks if string ends with inputted suffix

--- no_endswith.py
+++ no_endswith.py
@@ -5,3 +5,13 @@
 string = input("Enter a sentence: ")
 
 # Print 'True' if string ends with a certain parameter without endswith()
+
+def endswith_alternative(s, suffix):
+    if len(suffix) <= len(s):
+        result = s[-len(suffix):] == suffix
+    else: 
+        False
+    return result
+
+suffix = input("What should this string end with? Enter: ")
+print(f"Does it end with '{suffix}'?", endswith_alternative(string, suffix))
