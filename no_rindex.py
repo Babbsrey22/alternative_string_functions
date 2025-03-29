e13971fd8776af49ddedf0ace9c8c6bf28077293
Eunice <reyes05eunice@gmail.com>
2025-03-29 21:13:15 +0800

    Pseudocode

---
+++ no_rindex.py
@@ -0,0 +1,5 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. 
+# Create a program that do the same functionality without using rindex() function.
+
+# Input string
+# Find and count character in string STARTING FROM RIGHT (last character)
---
+++ no_upper.py
@@ -0,0 +1,5 @@
+# Prog03. upper() converts all characters of the string into upper case. 
+# Create a program that do the same functionality without using upper() function.
+
+# Input string
+# Convert all lowercase characters to uppercase without upper()
